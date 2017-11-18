d4d105c0180e1a02396f00f924679ac0a84a8654
Valentin Popov <info@valentineus.link>
2017-11-18 08:35:16 +0400

    Change version

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined("MOODLE_INTERNAL") || die();
 
-$plugin->release   = "2.0.0 (Build: 2017111800)";
-$plugin->version   = 2017111800;
+$plugin->release   = "2.1.0 (Build: 2017111810)";
+$plugin->version   = 2017111810;
 $plugin->requires  = 2016112900;
 $plugin->component = "local_webhooks";
 $plugin->maturity  = MATURITY_STABLE;
