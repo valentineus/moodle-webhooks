482d15db8f89b72e95c9e023d13cc6864b718f7e
Valentin Popov <info@valentineus.link>
2020-05-16 01:59:03 +0400

    Bump version
    Signed-off-by: Valentin Popov <info@valentineus.link>

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_webhooks';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '3.0.4 (Build: 2020041400)';
+$plugin->release = '3.0.5 (Build: 2020051600)';
 $plugin->requires = 2019052000;
-$plugin->version = 2020041400;
+$plugin->version = 2020051600;
